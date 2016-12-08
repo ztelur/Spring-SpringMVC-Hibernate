58455867d4f4ceb587bfffca36a541f703094f28
zhangtou88 <zhangkeyi1994@163.com>
2016-12-08 19:48:55 +0800

    分离Repo（专门用来处理分页和筛选排序)和Dao

--- SpringMVC/src/main/java/com/nju/ee/DAO/ArticleDao.java
+++ SpringMVC/src/main/java/com/nju/ee/DAO/ArticleDao.java
@@ -1,8 +1,6 @@
 package com.nju.ee.DAO;
 
 import com.nju.ee.entity.Article;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,13 +8,9 @@ import java.util.List;
  * Created by 克崽兽 on 2016/12/3.
  */
 public interface ArticleDao {
-    Article findOne(Integer integer);
+    Article getArticleById(int id);
 
     List<Article> getArticleWithPage(int page);
-
-    Page<Article> findAll(Pageable pageable);
-
-    <S extends Article> S save(S s);
 
     /**
      *
--- SpringMVC/src/main/java/com/nju/ee/DAO/ArticleDaoImpl.java
+++ SpringMVC/src/main/java/com/nju/ee/DAO/ArticleDaoImpl.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class ArticleDaoImpl implements ArticleDao {
     @Autowired
     private BaseDao baseDao;
-
+    @Override
     public Article getArticleById(int id){
         Session session=baseDao.getSession();
         Criteria cri=session.createCriteria(Article.class);
@@ -30,7 +30,6 @@ public class ArticleDaoImpl implements ArticleDao {
             return list.get(0);
         }
         return null;
-
     }
 
 
@@ -59,53 +58,11 @@ public class ArticleDaoImpl implements ArticleDao {
         baseDao.delete(article);
     }
 
-    public Iterable<Article> findAll(Sort sort) {
-        return null;
-    }
 
-    public Page<Article> findAll(Pageable pageable) {
-        return null;
-    }
-
-    public <S extends Article> Iterable<S> save(Iterable<S> iterable) {
-        return null;
-    }
-
-    public <S extends Article> S save(S s) {
-        return null;
-    }
 
     @Override
     public String delete4Result(Integer integer) {
         return null;
-    }
-
-    public Article findOne(Integer integer) {
-        return null;
-    }
-
-    public boolean exists(Integer integer) {
-        return false;
-    }
-
-    public Iterable<Article> findAll() {
-        return null;
-    }
-
-    public Iterable<Article> findAll(Iterable<Integer> iterable) {
-        return null;
-    }
-
-    public long count() {
-        return 0;
-    }
-
-    public void delete(Integer integer) {
-
-    }
-
-    public void delete(Iterable<? extends Article> iterable) {
-
     }
 
     public void deleteAll() {

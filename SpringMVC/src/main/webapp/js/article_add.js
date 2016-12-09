9b091be2be11402c4e303570a00f2fce5af4f36c
njushaoyang <651064360@qq.com>
2016-12-09 16:33:56 +0800

    reset editor and upload image

--- SpringMVC/src/main/webapp/js/article_add.js
+++ SpringMVC/src/main/webapp/js/article_add.js
@@ -1,18 +1,6 @@
-$('#edit').froalaEditor({
-    charCounterMax:10000,
-    heightMin:400,
-    // Set the image upload URL.
-    imageUploadURL: '/files/img',
-    // Set request type.
-    imageUploadMethod: 'POST',
-    imageUploadParam:'file',
-    // Set max image size to 5MB.
-    imageMaxSize: 5 * 1024 * 1024,
-    // Allow to upload PNG and JPG.
-    imageAllowedTypes: ['jpeg', 'jpg', 'png']
-});
+
 function reset(){
-    console.log(reset);
+    $('#edit').froalaEditor('html.set', '');
 };
 function checkForm(){
     return true;

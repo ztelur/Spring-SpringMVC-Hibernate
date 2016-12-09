4d3abee9f900414885bfc32bddc3f9aeb4d5dff0
njushaoyang <651064360@qq.com>
2016-12-09 15:49:24 +0800

    add_article.jsp

--- SpringMVC/src/main/webapp/js/article_add.js
+++ SpringMVC/src/main/webapp/js/article_add.js
@@ -2,9 +2,10 @@ $('#edit').froalaEditor({
     charCounterMax:10000,
     heightMin:400,
     // Set the image upload URL.
-    imageUploadURL: '/upload_image',
+    imageUploadURL: '/files/img',
     // Set request type.
     imageUploadMethod: 'POST',
+    imageUploadParam:'file',
     // Set max image size to 5MB.
     imageMaxSize: 5 * 1024 * 1024,
     // Allow to upload PNG and JPG.

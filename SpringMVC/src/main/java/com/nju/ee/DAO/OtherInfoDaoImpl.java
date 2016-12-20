7900afbb408624fc80dd2de2228f5222f70a1e1f
nemo733 <915281733@qq.com>
2016-12-20 21:27:18 +0800

    otherInfo  后端

---
+++ SpringMVC/src/main/java/com/nju/ee/DAO/OtherInfoDaoImpl.java
@@ -0,0 +1,61 @@
+package com.nju.ee.DAO;
+
+import com.nju.ee.entity.OtherInfo;
+import com.sun.xml.internal.xsom.impl.WildcardImpl;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by zangxiaojie on 2016/12/20.
+ */
+@Repository
+public class OtherInfoDaoImpl implements OtherInfoDao {
+
+    @Autowired
+    private BaseDao baseDao;
+
+    @Override
+    public OtherInfo getOtherInfo() {
+        Session session=baseDao.getSession();
+        Criteria cri=session.createCriteria(OtherInfo.class);
+        List<OtherInfo> list=cri.list();
+        if(list!=null){
+            return list.get(0);
+        }
+        else
+            System.out.println("Error:Other info is null!");
+        return null;
+    }
+
+    @Override
+    public OtherInfo update(OtherInfo otherInfo)  {
+        OtherInfo o;
+
+        try {
+            baseDao.update(otherInfo);
+            o=otherInfo;
+        } catch (Exception e) {
+            o=null;
+            e.printStackTrace();
+        }
+        return o;
+    }
+
+    @Override
+    public OtherInfo save(OtherInfo otherInfo) {
+        OtherInfo o;
+
+        try {
+            baseDao.save(otherInfo);
+            o=otherInfo;
+        } catch (Exception e) {
+            o=null;
+            e.printStackTrace();
+        }
+        return o;
+    }
+}
---
+++ SpringMVC/src/main/java/com/nju/ee/service/OtherInfoServiceImpl.java
@@ -0,0 +1,51 @@
+package com.nju.ee.service;
+
+import com.nju.ee.DAO.OtherInfoDao;
+import com.nju.ee.entity.OtherInfo;
+import com.nju.ee.vo.Error;
+import com.nju.ee.vo.OtherInfoVo;
+import com.nju.ee.vo.RestResult;
+import com.sun.xml.internal.xsom.impl.WildcardImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zangxiaojie on 2016/12/20.
+ */
+@Service
+public class OtherInfoServiceImpl implements OtherInfoService {
+
+    @Autowired
+    private OtherInfoDao otherInfoDao;
+
+    @Override
+    public RestResult getOtherInfo() {
+        OtherInfo otherInfo=otherInfoDao.getOtherInfo();
+        if (otherInfo ==null){
+            return RestResult.CreateResult(0,new Error(Error.BAD_PARAM,"不存在其他信息"));
+        }
+        OtherInfoVo otherInfoVo=new OtherInfoVo(otherInfo);
+        return RestResult.CreateResult(1,otherInfoVo);
+    }
+
+    @Override
+    public RestResult modifyOtherInfo(OtherInfoVo otherInfoVo) {
+        if (otherInfoVo ==null){
+            return RestResult.CreateResult(0,new Error(Error.BAD_PARAM,"不存在其他信息"));
+        }
+        OtherInfo otherInfo=otherInfoDao.getOtherInfo();
+        if (otherInfo ==null){
+            return RestResult.CreateResult(0,new Error(Error.BAD_PARAM,"不存在其他信息"));
+        }
+        otherInfo.setEmail(otherInfoVo.getEmail());
+        otherInfo.setLongitude(otherInfoVo.getLongitude());
+        otherInfo.setLatitude(otherInfoVo.getLatitude());
+
+        OtherInfo updateInfo=otherInfoDao.update(otherInfo);
+        if (updateInfo ==null){
+            return RestResult.CreateResult(0,new Error(Error.BAD_PARAM,"更新其他信息失败"));
+        }
+        OtherInfoVo vo=new OtherInfoVo(otherInfo);
+        return RestResult.CreateResult(1,vo);
+    }
+}
